98660778075adf9e9cf32ced79f753d21f4c44fa
Quinn Slack <qslack@qslack.com>
2018-11-02 03:25:13 -0700

    wip

---
+++ src/lsp.ts
@@ -0,0 +1,161 @@
+// tslint:disable-next-line:rxjs-no-wholesale
+import { from, Subscribable, Subscription } from 'rxjs'
+import { bufferCount, startWith } from 'rxjs/operators'
+import * as sourcegraph from 'sourcegraph'
+import { Unsubscribable } from 'sourcegraph'
+import * as rpc from 'vscode-jsonrpc'
+import {
+    InitializedNotification,
+    InitializeParams,
+    InitializeRequest,
+    MarkupKind,
+} from 'vscode-languageserver-protocol'
+import { prepURI } from './sourcegraph-python'
+import { createWebSocketMessageTransports } from './websocket'
+
+export class LanguageServerConnectionManager implements Unsubscribable {
+    private subscriptions = new Subscription()
+    private conns = new Map<string, Promise<rpc.MessageConnection>>()
+
+    constructor(
+        roots: Subscribable<ReadonlyArray<sourcegraph.WorkspaceRoot>>,
+        private prepareConn: (
+            rootURI: string,
+            conn: rpc.MessageConnection
+        ) => Promise<void>,
+        private address: string
+    ) {
+        // Initiate language server connection for all roots immediately when they are added.
+        this.subscriptions.add(
+            from(roots)
+                .pipe(
+                    startWith([] as sourcegraph.WorkspaceRoot[]),
+                    bufferCount(2)
+                )
+                .subscribe(([prevRoots, roots]) => {
+                    for (const prevRoot of prevRoots) {
+                        if (
+                            !roots
+                                .map(({ uri }) => uri.toString())
+                                .includes(prevRoot.uri.toString())
+                        ) {
+                            this.removeConnection(prevRoot.uri.toString())
+                        }
+                    }
+
+                    for (const { uri } of roots) {
+                        // It is safe to not handle the promise here, because all callers to getConnection will handle it.
+                        this.getOrCreateConnection(uri.toString())
+                    }
+                })
+        )
+
+        // Clean up connections.
+        this.subscriptions.add(() => {
+            for (const rootURI of this.conns.keys()) {
+                this.removeConnection(rootURI)
+            }
+            this.conns.clear()
+        })
+    }
+
+    private removeConnection(rootURI: string): void {
+        const conn = this.conns.get(rootURI)
+        this.conns.delete(rootURI)
+        if (conn) {
+            conn.then(c => c.dispose()).catch(err =>
+                console.error(
+                    `Error disposing Python language server connection for ${JSON.stringify(
+                        rootURI
+                    )}:`,
+                    err
+                )
+            )
+        }
+    }
+
+    public getConnection(uri: sourcegraph.URI): Promise<rpc.MessageConnection> {
+        // TODO!(sqs) HACK: chop off path to get root URI
+        const rootURI =
+            'git://github.com/sgtest/python-sample-0?ad924954afa36439105efa03cce4c5981a2a5384' // uri.toString().replace(/#.*$/, '')
+        return this.getOrCreateConnection(rootURI)
+    }
+
+    private async getOrCreateConnection(
+        rootURI: string
+    ): Promise<rpc.MessageConnection> {
+        let conn = this.conns.get(rootURI)
+        if (!conn) {
+            const { reader, writer } = await createWebSocketMessageTransports(
+                new WebSocket(this.address)
+            )
+            const c = rpc.createMessageConnection(reader, writer)
+            c.listen()
+            conn = initialize(prepURI(rootURI).toString(), c)
+                .then(() => this.prepareConn(rootURI, c))
+                .then(() => c)
+            this.conns.set(rootURI, conn)
+        }
+        return conn
+    }
+
+    public unsubscribe(): void {
+        this.subscriptions.unsubscribe()
+    }
+}
+
+async function initialize(
+    rootURI: string,
+    conn: rpc.MessageConnection
+): Promise<void> {
+    await conn.sendRequest(InitializeRequest.type, {
+        processId: null as any,
+        documentSelector: [{ language: 'python', scheme: 'git' }],
+        rootUri: rootURI,
+        workspaceFolders: [
+            {
+                name: rootURI,
+                uri: rootURI,
+            },
+        ],
+        capabilities: {
+            textDocument: {
+                hover: { contentFormat: [MarkupKind.Markdown] },
+            },
+            workspace: {
+                workspaceFolders: true,
+            },
+        },
+        initializationOptions: {
+            interpreter: {
+                properties: {
+                    InterpreterPath:
+                        '/home/sqs/.pyenv/versions/3.7.1/bin/python',
+                    Version: '3.7.1',
+                    DatabasePath:
+                        '/home/sqs/src/github.com/Microsoft/vscode-python/languageServer.0.1.48',
+                },
+            },
+            displayOptions: {
+                preferredFormat: 'markdown',
+                trimDocumentationLines: false,
+                maxDocumentationLineLength: 0,
+                trimDocumentationText: false,
+                maxDocumentationTextLength: 0,
+            },
+            testEnvironment: false,
+            searchPaths: [
+                '/usr/lib/python3.7;/usr/lib/python3.7/plat-x86_64-linux-gnu;/usr/lib/python3.7/lib-tk;/usr/lib/python3.7/lib-old;/usr/lib/python3.7/lib-dynload;/home/sqs/.local/lib/python3.7/site-packages;/usr/local/lib/python3.7/dist-packages;/usr/lib/python3.7/dist-packages;/usr/lib/python3.7/dist-packages/gtk-2.0;',
+                '/usr/bin',
+            ],
+            typeStubSearchPaths: [
+                '/home/sqs/src/github.com/Microsoft/vscode-python/languageServer.0.1.48/Typeshed',
+            ],
+            excludeFiles: [],
+            analysisUpdates: true,
+            traceLogging: true, // Max level, let LS decide through settings actual level of logging.
+            asyncStartup: true,
+        },
+    } as InitializeParams)
+    conn.sendNotification(InitializedNotification.type)
+}
--- src/sourcegraph-python.ts
+++ src/sourcegraph-python.ts
@@ -1,110 +1,87 @@
+// tslint:disable-next-line:rxjs-no-wholesale
+import { from } from 'rxjs'
+import { map, startWith } from 'rxjs/operators'
 import * as sourcegraph from 'sourcegraph'
 import * as rpc from 'vscode-jsonrpc'
 import {
+    DefinitionRequest,
     DidOpenTextDocumentNotification,
     DidOpenTextDocumentParams,
     HoverRequest,
-    InitializedNotification,
-    InitializeParams,
-    InitializeRequest,
-    MarkupKind,
+    ReferenceParams,
+    ReferencesRequest,
+    TextDocumentPositionParams,
 } from 'vscode-languageserver-protocol'
-import { createWebSocketMessageTransports } from './websocket'
+import { LanguageServerConnectionManager } from './lsp'
 
-async function connectTo2(address: string): Promise<rpc.MessageConnection> {
-    const { reader, writer } = await createWebSocketMessageTransports(
-        new WebSocket(address)
+const ADDR = 'ws://localhost:4288'
+
+export function prepURI(uri: string | sourcegraph.URI): sourcegraph.URI {
+    // return new sourcegraph.URI(uri.toString())
+    return new sourcegraph.URI(
+        uri
+            .toString()
+            .replace('?', '/')
+            .replace('#', '/')
     )
-    return rpc.createMessageConnection(reader, writer)
+}
+
+function unprepURI(uri: string | sourcegraph.URI): string {
+    return uri
+        .toString()
+        .replace('b6f5/', 'b6f5#')
+        .replace('/fd3759', '?fd3759')
+        .replace('b09b/', 'b09b#')
+        .replace('/7e71', '?7e71')
 }
 
 export async function activate(): Promise<void> {
-    const conn = await connectTo2('ws://localhost:4288')
-    conn.listen()
+    const conns = new LanguageServerConnectionManager(
+        from(
+            sourcegraph.workspace.onDidChangeRoots as any /* TODO!(sqs) */
+        ).pipe(
+            startWith(void 0),
+            map(() => sourcegraph.workspace.roots)
+        ),
+        sendFiles,
+        ADDR
+    )
 
-    const rootURI = `file:///home/sqs/src/${
-        sourcegraph.workspace.textDocuments[0]
-            ? new URL(
-                  sourcegraph.workspace.textDocuments[0].uri
-              ).pathname.slice(2)
-            : 'github.com/sgtest/python-single-package-user'
-    }`
-
-    await conn.sendRequest(InitializeRequest.type, {
-        processId: null as any,
-        documentSelector: [{ language: 'python', scheme: 'file' }],
-        rootUri: rootURI,
-        workspaceFolders: [
-            {
-                name: 'python-sample-0',
-                uri: rootURI,
-            },
-        ],
-        capabilities: {
-            textDocument: {
-                hover: { contentFormat: [MarkupKind.Markdown] },
-            },
-            workspace: {
-                workspaceFolders: true,
-            },
-        },
-        initializationOptions: {
-            interpreter: {
-                properties: {
-                    InterpreterPath:
-                        '/home/sqs/.pyenv/versions/3.7.1/bin/python',
-                    Version: '3.7.1',
-                    DatabasePath:
-                        '/home/sqs/src/github.com/Microsoft/vscode-python/languageServer.0.1.48',
-                },
-            },
-            displayOptions: {
-                preferredFormat: 'markdown',
-                trimDocumentationLines: false,
-                maxDocumentationLineLength: 0,
-                trimDocumentationText: false,
-                maxDocumentationTextLength: 0,
-            },
-            testEnvironment: false,
-            searchPaths: [
-                '/usr/lib/python3.7;/usr/lib/python3.7/plat-x86_64-linux-gnu;/usr/lib/python3.7/lib-tk;/usr/lib/python3.7/lib-old;/usr/lib/python3.7/lib-dynload;/home/sqs/.local/lib/python3.7/site-packages;/usr/local/lib/python3.7/dist-packages;/usr/lib/python3.7/dist-packages;/usr/lib/python3.7/dist-packages/gtk-2.0;',
-                '/usr/bin',
-            ],
-            typeStubSearchPaths: [
-                '/home/sqs/src/github.com/Microsoft/vscode-python/languageServer.0.1.48/Typeshed',
-            ],
-            excludeFiles: [],
-            analysisUpdates: true,
-            traceLogging: true, // Max level, let LS decide through settings actual level of logging.
-            asyncStartup: true,
-        },
-    } as InitializeParams)
-    conn.sendNotification(InitializedNotification.type)
-
-    const toURI = (doc: sourcegraph.TextDocument) => {
-        const docuri = new URL(doc.uri)
-
-        // TODO pass a Zip URL instead of hard-coding the path on disk
-        const fetchuri = `file:///home/sqs/src/${docuri.pathname.slice(
-            2
-        )}/${docuri.hash.slice(1)}`
-        return fetchuri
+    async function sendFiles(
+        rootURI: string,
+        conn: rpc.MessageConnection
+    ): Promise<void> {
+        await new Promise(resolve => setTimeout(resolve, 1000))
+        const uris = await listFilesRecursively(
+            sourcegraph.workspace.fileSystem,
+            new sourcegraph.URI(rootURI)
+        )
+        for (const uri of uris) {
+            if (
+                uri.toString().endsWith('.py') &&
+                !uri.toString().endsWith('setup.py')
+            ) {
+                const contents = await sourcegraph.workspace.fileSystem.readFile(
+                    uri
+                )
+                conn.sendNotification(DidOpenTextDocumentNotification.type, {
+                    textDocument: {
+                        uri: prepURI(uri).toString(),
+                        languageId: 'python',
+                        text: new TextDecoder('utf-8').decode(contents),
+                        version: 0,
+                    },
+                } as DidOpenTextDocumentParams)
+                await new Promise(resolve => setTimeout(resolve, 300))
+            }
+        }
     }
 
-    for (const doc of sourcegraph.workspace.textDocuments) {
+    sourcegraph.workspace.onDidOpenTextDocument.subscribe(async doc => {
+        const conn = await conns.getConnection(new sourcegraph.URI(doc.uri))
         conn.sendNotification(DidOpenTextDocumentNotification.type, {
             textDocument: {
-                uri: toURI(doc),
-                languageId: doc.languageId,
-                text: doc.text,
-                version: 0,
-            },
-        } as DidOpenTextDocumentParams)
-    }
-    sourcegraph.workspace.onDidOpenTextDocument.subscribe(doc => {
-        conn.sendNotification(DidOpenTextDocumentNotification.type, {
-            textDocument: {
-                uri: toURI(doc),
+                uri: prepURI(doc.uri).toString(),
                 languageId: doc.languageId,
                 text: doc.text,
                 version: 0,
@@ -114,9 +91,10 @@ export async function activate(): Promise<void> {
 
     sourcegraph.languages.registerHoverProvider(['python'], {
         provideHover: async (doc, pos) => {
+            const conn = await conns.getConnection(new sourcegraph.URI(doc.uri))
             const response = await conn.sendRequest(HoverRequest.type, {
                 textDocument: {
-                    uri: toURI(doc),
+                    uri: prepURI(doc.uri).toString(),
                 },
                 position: {
                     line: pos.line,
@@ -124,22 +102,94 @@ export async function activate(): Promise<void> {
                 },
             })
 
-            console.log('XXX', await conn.sendRequest())
-
-            if (!response) {
-                return {
-                    contents: {
-                        value: '```go\n' + 'NO RESPONSE' + '\n```',
-                        kind: sourcegraph.MarkupKind.Markdown,
-                    },
-                }
-            }
-
-            return {
-                contents: response.contents,
-                // contents: { value: '' },
-                // __backcompatContents: response.contents,
-            } as sourcegraph.Hover
+            return response
+                ? ({ contents: response.contents } as sourcegraph.Hover)
+                : null
         },
     })
+
+    sourcegraph.languages.registerDefinitionProvider(['python'], {
+        provideDefinition: async (doc, pos) => {
+            const conn = await conns.getConnection(new sourcegraph.URI(doc.uri))
+            const response = await conn.sendRequest(DefinitionRequest.type, {
+                textDocument: {
+                    uri: prepURI(doc.uri).toString(),
+                },
+                position: {
+                    line: pos.line,
+                    character: pos.character,
+                },
+            } as TextDocumentPositionParams)
+            return response
+                ? Array.isArray(response)
+                    ? response.map(loc => ({
+                          uri: new sourcegraph.URI(unprepURI(loc.uri)),
+                          range: new sourcegraph.Range(
+                              loc.range.start.line,
+                              loc.range.start.character,
+                              loc.range.end.line,
+                              loc.range.end.character
+                          ),
+                      }))
+                    : null
+                : null
+        },
+    })
+
+    sourcegraph.languages.registerReferenceProvider(['python'], {
+        provideReferences: async (doc, pos, context) => {
+            const conn = await conns.getConnection(new sourcegraph.URI(doc.uri))
+            const response = await conn.sendRequest(ReferencesRequest.type, {
+                textDocument: {
+                    uri: prepURI(doc.uri).toString(),
+                },
+                position: {
+                    line: pos.line,
+                    character: pos.character,
+                },
+                context,
+            } as ReferenceParams)
+            return response
+                ? response.map(loc => ({
+                      uri: new sourcegraph.URI(unprepURI(loc.uri)),
+                      range: new sourcegraph.Range(
+                          loc.range.start.line,
+                          loc.range.start.character,
+                          loc.range.end.line,
+                          loc.range.end.character
+                      ),
+                  }))
+                : null
+        },
+    })
+}
+
+async function listFilesRecursively(
+    fs: sourcegraph.FileSystem,
+    rootURI: sourcegraph.URI
+): Promise<sourcegraph.URI[]> {
+    const files: sourcegraph.URI[] = []
+    const toRecurse: sourcegraph.URI[] = [new sourcegraph.URI(`${rootURI}#`)]
+    while (toRecurse.length > 0) {
+        const dir = toRecurse.shift()!
+        const entries = await fs.readDirectory(dir)
+        for (const [name, type] of entries) {
+            const uri = new sourcegraph.URI(
+                `${dir.toString()}${
+                    dir.toString().endsWith('#') ? '' : '/'
+                }${name}`
+            )
+            switch (type) {
+                case sourcegraph.FileType.File:
+                    files.push(uri)
+                    break
+                case sourcegraph.FileType.Directory:
+                    toRecurse.push(uri)
+                    break
+                default:
+                // TODO(sqs): skip symlinks and unknown
+            }
+        }
+    }
+    return files
 }
